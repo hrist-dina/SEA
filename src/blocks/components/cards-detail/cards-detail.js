b8cfeff8d88fddc7f96a5389079bf257baf94a98
Dina Hrist <hdina@inbox.ru>
2020-03-25 22:02:44 +0700

    Done card detail

---
+++ src/blocks/components/cards-detail/cards-detail.js
@@ -0,0 +1,26 @@
+import $ from "jquery";
+import ComponentBase from "%classes%/ComponentBase";
+
+export class CardsDetail extends ComponentBase {
+    constructor(selector = ".js-scrollto") {
+        super(selector);
+    }
+
+    init() {
+        $(this.selector).on("click", e => {
+            let href = $(e.target)
+                .closest(this.selector)
+                .attr("href");
+
+            $("html, body").animate(
+                {
+                    scrollTop: $(href).offset().top,
+                },
+                {
+                    duration: 370,
+                    easing: "linear",
+                }
+            );
+        });
+    }
+}
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -2,9 +2,11 @@ import $ from "jquery";
 import { UiKit } from "%components%/ui-kit/ui-kit";
 import PCard from "%components%/p-card/p-card";
 import Card from "%components%/card/card";
+import {CardsDetail} from "%components%/cards-detail/cards-detail";
 
 $(() => {
     new Card();
     new PCard();
     new UiKit();
+    new CardsDetail();
 });
