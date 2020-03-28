31c25da9402ba5543b0f0e8c02b30021aa2ca5db
Dina Hrist <hdina@inbox.ru>
2020-03-28 17:23:23 +0700

    Added mobile menu and search in header

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -8,7 +8,14 @@ export default class Header extends ComponentBase {
 
     init() {
         this.classMenuOpen = `${this.selector}-menu-open`;
+
+        this.classSearch = `${this.selector}-search`;
+        this.classSearchOpen = `${this.classSearch}-open`;
+        this.classSearchClose = `${this.classSearch}-close`;
+
         $(this.classMenuOpen).on("click", this.clickMenuOpen.bind(this));
+        $(this.classSearchOpen).on("click", this.clickSearchOpen.bind(this));
+        $(this.classSearchClose).on("click", this.clickSearchClose.bind(this));
     }
 
     clickMenuOpen(e) {
@@ -16,5 +23,15 @@ export default class Header extends ComponentBase {
         el.toggleClass("active").removeClass("active-fixed");
         el.closest(this.selector).toggleClass("active");
         el.closest("body").toggleClass("o-hidden");
+    }
+
+    clickSearchOpen(e) {
+        const el = $(e.target);
+        el.closest(this.classSearch).addClass("active");
+    }
+
+    clickSearchClose(e) {
+        const el = $(e.target);
+        el.closest(this.classSearch).removeClass("active");
     }
 }
