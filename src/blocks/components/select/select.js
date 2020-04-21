63936ad94a4cff6aec265b63df09ceae65b7e444
Dina Hrist <hdina@inbox.ru>
2020-04-21 23:52:46 +0700

    Try fix open select2 on iOs

--- src/blocks/components/select/select.js
+++ src/blocks/components/select/select.js
@@ -1,6 +1,6 @@
 import $ from "jquery"
 // eslint-disable-next-line
-import select2 from "select2";
+import select2 from "select2"
 // Initialise imported function as jQuery function
 $.fn.select2.defaults.set("width", "100%")
 
@@ -11,7 +11,7 @@ export default class Select {
 
         this.baseOptions = {
             minimumResultsForSearch: Infinity,
-            theme: this.theme
+            theme: this.theme,
         }
 
         this.options = $.extend(this.baseOptions, options)
@@ -21,5 +21,14 @@ export default class Select {
 
     init() {
         $(this.selector).select2(this.options)
+
+        $(document).on("select2:opening select2:open", () => {
+            if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+                setTimeout(() => {
+                    console.log(document.activeElement)
+                    document.activeElement.blur()
+                }, 1)
+            }
+        })
     }
 }
